a572aae801f3bd488e2adfe3fcdf747a2ba7f4a1
堀口誠人 <horiguchi_seito@liginc.co.jp>
2015-02-23 00:41:07 +0900

    共通部分WP　とりあえずおｋ

---
+++ app/theme-clear-box/footer.php
@@ -0,0 +1,14 @@
+            <!--footer ここから-->
+            <footer class="footer">
+                <small>Copyright &copy; <a href="https://twitter.com/seito_horiguchi" target="_blank">Horiguchi Seito(Japanese Web developer)</a></small>
+            </footer>
+            <!--footer ここまで-->
+        </div>
+        <!--wrapper ここまで-->
+        <!--javascript ここから-->
+        <script async src="//assets.codepen.io/assets/embed/ei.js"></script>
+        <script src="<?php bloginfo('template_directory'); ?>/js/library.js"></script>
+        <script src="<?php bloginfo('template_directory'); ?>/js/app.js"></script>
+        <!--javascript ここまで-->
+    </body>
+</html>
---
+++ app/theme-clear-box/header.php
@@ -0,0 +1,35 @@
+        <!--
+            javascriptを有効にしていなユーザーへのメッセージ。
+            対応外ブラウザを使用しているユーザーへのメッセージ。
+        -->
+        <noscript>Javascriptを有効にしてください。</noscript>
+        <div class="for-old">お使いのOS・ブラウザでは本サイトを閲覧することができません。</div>
+
+        <!--jsでパスの値が必要な場合に使うタグ-->
+        <input type="hidden" value="./" id="base-url">
+
+        <!--wrapper ここから-->
+        <div class="wrapper" id="js-wrapper">
+
+            <!--header ここから-->
+            <header class="header">
+                <!--nav ここから-->
+                <nav class="nav">
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <a href="#">Top</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#">Components</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#">License & Author</a>
+                        </li>
+                    </ul>
+                </nav>
+                <!--nav ここまで-->
+            </header>
+            <!--header ここまで-->
