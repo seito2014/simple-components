240ed5fca295877015e82c5d1c14ed70036bece7
堀口誠人 <horiguchi_seito@liginc.co.jp>
2015-04-05 21:25:55 +0900

    検索hit数表示

--- app/wp/wp-content/themes/theme-clear-box/search.php
+++ app/wp/wp-content/themes/theme-clear-box/search.php
@@ -15,7 +15,11 @@ Template Name: Search Page
         <h1 class="heading">
             Results:
             "<?php the_search_query(); ?>"
-            <?php global $wp_query;$total_results = $wp_query->found_posts;?>
+            <?php
+            global $wp_query;
+            $total_results = $wp_query->found_posts;
+            ?>
+            <?php echo $wp_query->found_posts; ?>
             Hit
         </h1>
 
@@ -26,46 +30,46 @@ Template Name: Search Page
         ?>
 
         <div class="section-contents">
-            <?php
-            global $wp_query;
-            query_posts(array_merge(
-                array( 'post_type' => array('post', 'code') ),
-                $wp_query->query
-            ));
-            if (have_posts()) : while (have_posts()) : the_post();
-                ?>
 
-                <ul class="card-list">
-                    <li class="card-item">
-                        <article class="card">
-                            <div class="comment">
-                                <?php the_content(); ?>
-                                <div class="comment-arrow"></div>
-                            </div>
+            <ul class="card-list">
 
-                            <div class="card-embed">
-                                <p data-height="268" data-theme-id="0" data-slug-hash="<?php echo substr(get_the_excerpt(),32,39) ?>"
-                                   data-default-tab="result" data-user="seito2014" class='codepen'>
-                                    See the Pen <a href='<?php the_excerpt(); ?>'><?php echo substr(get_the_excerpt(),32,39) ?></a> by Seito (<a
-                                        href='http://codepen.io/seito2014'>@seito2014</a>) on
-                                    <a href='http://codepen.io'>CodePen</a>.
-                                </p>
-                            </div>
-                            <div class="card-body">
-                                <h1 class="card-heading">
-                                    <a href="<?php the_excerpt(); ?>" target="_blank"><?php the_title(); ?></a>
-                                </h1>
-                                <ul class="card-tag-list">
-                                    <?php the_tags('<li class="card-tag-item">', '</li><li class="card-tag-item">', '</li>'); ?>
-                                </ul>
-                                <footer class="card-footer">
-                                    <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y/m/d'); ?></time>
-                                </footer>
-                            </div>
-                        </article>
-                    </li>
-                </ul>
-            <?php endwhile; ?>
+                <?php global $wp_query;
+                query_posts(array_merge(
+                    array( 'post_type' => array('post', 'code') ),
+                    $wp_query->query
+                ));
+                if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <li class="card-item">
+                    <article class="card">
+                        <div class="comment">
+                            <?php the_content(); ?>
+                            <div class="comment-arrow"></div>
+                        </div>
+
+                        <div class="card-embed">
+                            <p data-height="268" data-theme-id="0" data-slug-hash="<?php echo substr(get_the_excerpt(),32,39) ?>"
+                               data-default-tab="result" data-user="seito2014" class='codepen'>
+                                See the Pen <a href='<?php the_excerpt(); ?>'><?php echo substr(get_the_excerpt(),32,39) ?></a> by Seito (<a
+                                    href='http://codepen.io/seito2014'>@seito2014</a>) on
+                                <a href='http://codepen.io'>CodePen</a>.
+                            </p>
+                        </div>
+                        <div class="card-body">
+                            <h1 class="card-heading">
+                                <a href="<?php the_excerpt(); ?>" target="_blank"><?php the_title(); ?></a>
+                            </h1>
+                            <ul class="card-tag-list">
+                                <?php the_tags('<li class="card-tag-item">', '</li><li class="card-tag-item">', '</li>'); ?>
+                            </ul>
+                            <footer class="card-footer">
+                                <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y/m/d'); ?></time>
+                            </footer>
+                        </div>
+                    </article>
+                </li>
+                <?php endwhile; ?>
+
+            </ul>
 
             <?php else : ?>
                 <p class="nothing-text">"<?php the_search_query(); ?>" is nothing.</p>
